008e86dfec1491099937e73754b99c724e87e352
Andrew <phongnguyentuan20@gmail.com>
2021-06-28 22:27:07 -0400

    added @testing-library/cypress

--- cypress/integration/examples/hello.spec.ts
+++ cypress/integration/examples/hello.spec.ts
@@ -1,14 +1,8 @@
 describe('My First Test', () => {
   it('should visits the kitchen sink', () => {
-    cy.visit('https://example.cypress.io');
-    cy.contains('type').click();
-
-    // Should be on a new URL which includes '/commands/actions'
-    cy.url().should('include', '/commands/actions');
-
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('.action-email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com');
+    cy.visit('https://vasseneca.com/');
+    cy.findByRole('heading', {
+      name: /Vietnamese Association at Seneca College/,
+    }).should('exist');
   });
 });
