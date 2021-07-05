67031a1071278eab99e1c6dc3acd849ff99d94ff
Andrew <phongnguyentuan20@gmail.com>
2021-07-04 23:24:27 -0400

    added WithLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import LoginForm from '@modules/user/LoginForm/LoginForm';
-import React from 'react';
+import { AppProps } from 'next/app';
+import React, { FC } from 'react';
 import DefaultLayout from 'src/layouts/DefaultLayout';
 
 export default function Home() {
   return (
-    <DefaultLayout>
+    <>
       <LoginForm />
-    </DefaultLayout>
+    </>
   );
 }
+
+Home.WithLayout = ((page: AppProps['Component']) => (
+  <DefaultLayout>{page}</DefaultLayout>
+)) as FC;
