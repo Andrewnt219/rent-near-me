e6a70cb3bc722e5bbd82096a381e7d89ce69b1b1
Khang Nguyen <khangngocnamnguyen@outlook.com>
2021-07-16 19:08:28 -0400

    Use service key on server

--- src/modules/user/RegisterForm/RegisterForm.tsx
+++ src/modules/user/RegisterForm/RegisterForm.tsx
@@ -1,14 +1,15 @@
+import useTranslation from 'next-translate/useTranslation';
 import { useLayoutModal } from '@layouts/LayoutModalContext';
 import DatePicker from '@libs/react-day-picker/DatePicker';
 import RegisterModel from '@models/RegisterForm';
 import { ButtonLink, ButtonPrimary } from '@ui/Button/Button';
+import { RiErrorWarningFill } from 'react-icons/ri';
 import Form from '@ui/Form';
 import PasswordCheckList from '@ui/PasswordCriteria/PasswordCriteria';
 import PasswordField from '@ui/PasswordField';
 import Row from '@ui/Row/Row';
 import Select from '@ui/SelectField';
 import TextField from '@ui/TextField';
-import useTranslation from 'next-translate/useTranslation';
 import useRegisterForm from './useRegisterForm';
 
 export default function RegisterForm() {
@@ -94,6 +95,13 @@ export default function RegisterForm() {
       <PasswordCheckList
         passwordValidationResults={passwordValidationResults}
       />
+
+      {submitError && (
+        <Form.ErrorMessage tw="text-body flex items-center gap-sm mb-sm">
+          <RiErrorWarningFill tw="w-5 h-5 fill-current" />
+          {submitError}
+        </Form.ErrorMessage>
+      )}
 
       <ButtonPrimary
         size="xl"
