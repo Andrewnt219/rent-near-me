a874137b887420d247db651c6c15a67674158525
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2021-10-31 01:52:21 -0400

    refactor: update tailwindcss tokens (#79)
    * fix: weird import from react
    
    See https://stackoverflow.com/questions/69038328/why-is-vs-code-importing-from-hoist-non-react-statics-node-modules-types-react
    
    * chore: update project vscode settings
    
    * update tailwindcss tokens
    
      * update colors
    
      * update shadow
    
      * update spacing
    
      * update font
    
      * update accessible colors
    
    * fix crossOrigin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { fontFamily, colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
@@ -11,40 +11,81 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     shadow: {
-      DEFAULT: '0px 6px 16px rgb(0 0 0 / 12%)',
-      sm: '0px 1px 2px rgb(0 0 0 / 8%), 0px 4px 12px rgb(0 0 0 / 5%)',
+      z1: 'rgb(145 158 171 / 24%) 0px 1px 2px 0px',
+      z8: 'rgb(145 158 171 / 24%) 0px 8px 16px 0px',
+      z12: 'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 12px 24px 0px',
+      z16: 'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 16px 32px -4px',
+      z20: 'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 20px 40px -4px',
+      z24: 'rgb(145 158 171 / 24%) 0px 0px 4px 0px, rgb(145 158 171 / 24%) 0px 24px 48px 0px',
+      primary: 'rgb(220 48 79 / 24%) 0px 8px 16px 0px',
+      secondary: 'rgb(0 132 137 / 24%) 0px 8px 16px 0px',
+      success: 'rgb(20 136 71 / 24%) 0px 8px 16px 0px',
+      info: 'rgb(38 99 255 / 24%) 0px 8px 16px 0px',
+      warning: 'rgb(172 100 12 / 24%) 0px 8px 16px 0px',
+      danger: 'rgb(188 30 93 / 24%) 0px 8px 16px 0px',
     },
     fontSize: {
-      hero: 'clamp(2.5rem, 2.1rem + 2vw, 4.5rem)', // 40~72px
-      h1: 'clamp(2rem, 1.6666666666666667rem + 1.6666666666666667vw, 3rem)', // 32~48px
-      h2: 'clamp(1.5rem, 1.1666666666666667rem + 1.6666666666666667vw, 2.5rem)', // 24~40px,
-      xl: '1.5rem',
-      h3: 'clamp(1.125rem, 0.8333333333333333rem + 1.4583333333333333vw, 2rem)', // 18~32px
-      lg: '1.125rem',
-      body: '1rem',
-      sm: '0.875rem',
-      xs: '0.75rem',
+      h1: ['clamp(2.5rem, 2.2rem + 1.5vw, 4rem)', '1.25'],
+      h2: ['clamp(2rem, 1.8rem + 1vw, 3rem)', '1.33333'],
+      h3: ['clamp(1.5rem, 1.4rem + 0.5vw, 2rem);', '1.5'],
+      h4: ['clamp(1.25rem, 1.2rem + 0.25vw, 1.5rem)', '1.5'],
+      h5: ['clamp(1.125rem, 1.1rem + 0.125vw, 1.25rem)', '1.5'],
+      h6: ['clamp(1.0625rem, 1.05rem + 0.0625vw, 1.125rem)', '1.55556'],
+      body1: ['1rem', '1.5'],
+      body2: ['0.875rem', '1.57143'],
+      caption: ['0.75rem', '1.5'],
+      overline: ['0.75rem', '1.5'],
+      button: ['0.875rem', '1.71429'],
     },
+
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      primary: {
-        DEFAULT: '#ff385c',
-        dark: '#bd1e59',
-      },
-      secondary: '#008489',
-      danger: '#dc2626',
-      success: '#008a05',
-      info: '#007bff',
       white: '#fff',
       black: '#000',
-      light: '#f7f7f7',
-      dark: '#333333',
-      gray: {
-        DEFAULT: '#9ca3af',
+      primary: {
+        DEFAULT: '#DC304F',
+        light: '#FDDDD5',
+        dark: '#69093E',
       },
-      bordercolor: '#dddddd',
+      secondary: {
+        DEFAULT: '#008489',
+        light: '#C7F9EA',
+        dark: '#002941',
+      },
+      success: {
+        DEFAULT: '#148847',
+        light: '#CEF9D0',
+        dark: '#034137',
+      },
+      info: {
+        DEFAULT: '#2663FF',
+        light: '#D3E4FF',
+        dark: '#071A7A',
+      },
+      warning: {
+        DEFAULT: '#ac640c',
+        light: '#FAEECC',
+        dark: '#521F02',
+      },
+      danger: {
+        DEFAULT: '#BC1E5D',
+        light: '#FBD1D1',
+        dark: '#5A0549',
+      },
+      gray: {
+        100: '#F9FAFB',
+        200: '#F4F6F8',
+        300: '#DFE3E8',
+        400: '#C4CDD5',
+        500: '#919EAB',
+        600: '#637381',
+        700: '#454F5B',
+        800: '#212B36',
+        900: '#161C24',
+      },
     },
+
     extend: {
       maxWidth: {
         '8xl': '90rem',
@@ -56,24 +97,24 @@ module.exports = {
       },
 
       borderColor: (theme) => ({
-        DEFAULT: theme('colors.bordercolor'),
+        DEFAULT: theme('colors.gray.300'),
       }),
 
       fontFamily: {
-        sans: ['Montserrat', ...fontFamily.sans],
+        sans: ['Public Sans', ...fontFamily.sans],
       },
 
       spacing: {
-        xs: '0.25rem',
-        sm: '0.5rem',
-        md: '1rem',
-        lg: '1.25rem',
-        xl: '1.5rem',
-        '2xl': '2rem',
-        '3xl': '3rem',
-        '4xl': '5rem',
-        'var-app-bar': 'var(--app-bar-height, 4.8125rem)',
-        'var-page-px': 'var(--page-px, 1.5rem)',
+        s1: '0.5rem',
+        s2: '1rem',
+        s3: '1.5rem',
+        s4: '2rem',
+        s5: '2.5rem',
+        s6: '3rem',
+        s7: '3.5rem',
+        s8: '4rem',
+        s9: '4.5rem',
+        s10: '5rem',
       },
     },
   },
@@ -98,7 +139,7 @@ function commons({ addComponents }) {
   });
 }
 
-function fonts({ addComponents, theme }) {
+function fonts({ addComponents }) {
   addComponents({
     '.font-inherit': {
       font: 'inherit',
