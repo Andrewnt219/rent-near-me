0ea3fd265fbbaae444299f0f53e3f88134e0e8d2
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2021-10-31 01:14:52 -0400

    fix crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,7 +41,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin=""
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
