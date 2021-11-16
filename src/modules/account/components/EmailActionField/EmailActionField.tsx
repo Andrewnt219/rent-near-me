f50e57c80fe902eae3b1f73ec0d43f3bf78e7945
Khang Nguyen <khang@khangnguyen.dev>
2021-11-15 21:18:51 -0500

    Send email upon registration

--- src/modules/account/components/EmailActionField/EmailActionField.tsx
+++ src/modules/account/components/EmailActionField/EmailActionField.tsx
@@ -2,13 +2,18 @@ import { VFC } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import ToggleActionField from '@modules/account/components/ActionField/ToggleActionField';
 import { useAuth } from '@modules/user-auth/contexts/AuthContext';
+import Text from '@ui/Text/Text';
+import { ButtonLink } from '@ui/Button/Button';
+import AuthApi from '@services/AuthApi';
+import { useSnackbar } from '@ui/Snackbar/SnackbarContext';
+import { getErrorMessage } from '@utils/api-responses';
 
 const EmailActionField: VFC = () => {
   const { t } = useTranslation();
 
   return (
     <ToggleActionField
-      label={t('account:security.change-email.field-label')}
+      label={t('account:security.email.field-label')}
       showActionButton={false}
       alternativeContent={<EmailActionFieldAltContent />}
     />
@@ -20,5 +25,46 @@ export default EmailActionField;
 const EmailActionFieldAltContent: VFC = () => {
   const { user } = useAuth();
 
-  return <>{user?.email}</>;
+  return (
+    <>
+      <Text tw="mb-sm">{user?.email}</Text>
+      {!user?.emailVerified && <EmailUnverifiedAlert />}
+    </>
+  );
+};
+
+const EmailUnverifiedAlert: VFC = () => {
+  const { t } = useTranslation();
+  const snackbar = useSnackbar();
+
+  const onSendEmailVerificationClick = async () => {
+    try {
+      await AuthApi.sendEmailVerification();
+      snackbar.showSnackSuccess(
+        t(
+          'account:security.email.description.emailUnverifiedAlert.message.success'
+        )
+      );
+    } catch (e) {
+      snackbar.showSnackError(getErrorMessage(e, t));
+    }
+  };
+
+  return (
+    // TODO(vito): Use a warning alert here when the component is built
+    <div>
+      <Text>
+        <span tw="font-bold">{t('common:fields.alert.warning')}</span>:{' '}
+        {t('account:security.email.description.emailUnverifiedAlert.line1')}
+      </Text>
+      <Text variant="body2">
+        {t('account:security.email.description.emailUnverifiedAlert.line2')}
+      </Text>
+      <Text variant="body2">
+        <ButtonLink onClick={onSendEmailVerificationClick}>
+          {t('account:security.email.description.emailUnverifiedAlert.line3')}
+        </ButtonLink>
+      </Text>
+    </div>
+  );
 };
