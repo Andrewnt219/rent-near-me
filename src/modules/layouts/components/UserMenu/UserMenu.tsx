f877572aab0720aeac6e0dececec691322e6bad0
Khang Nguyen <khang@khangnguyen.dev>
2021-12-08 18:22:49 -0500

    Fix UserMenu focus order issue

--- src/modules/layouts/components/UserMenu/UserMenu.tsx
+++ src/modules/layouts/components/UserMenu/UserMenu.tsx
@@ -11,50 +11,66 @@ import { useAuth } from '@modules/user-auth/contexts/AuthContext';
 import AuthApi from '@services/AuthApi';
 
 const UserMenu: VFC = () => {
-  const { t } = useTranslation();
   const { isAuthenticated } = useAuth();
-  const { registerModal, loginModal } = useModals();
-  const router = useRouter();
 
   return (
     <Menu button={<UserMenuButton />}>
       {isAuthenticated ? (
-        <MenuItemGroup>
-          <MenuLink href="/account">
-            {t('common:routes.account.index')}
-          </MenuLink>
-        </MenuItemGroup>
+        <AuthenticatedUserMenuContent />
       ) : (
-        <MenuItemGroup tw="font-semibold">
-          <MenuItem onSelect={registerModal?.show}>
-            {t('common:userMenu.register')}
-          </MenuItem>
-          <MenuItem onSelect={loginModal?.show}>
-            {t('common:userMenu.login')}
-          </MenuItem>
-        </MenuItemGroup>
+        <UnauthenticatedUserMenuContent />
       )}
+    </Menu>
+  );
+};
+
+const UnauthenticatedUserMenuContent: VFC = () => {
+  const { t } = useTranslation();
+  const { registerModal, loginModal } = useModals();
+  return (
+    <>
+      <MenuItemGroup tw="font-semibold">
+        <MenuItem onSelect={registerModal?.show}>
+          {t('common:userMenu.register')}
+        </MenuItem>
+        <MenuItem onSelect={loginModal?.show}>
+          {t('common:userMenu.login')}
+        </MenuItem>
+      </MenuItemGroup>
+      <MenuItemGroup label={t('common:userMenu.quickLinks')}>
+        <MenuLink href="/">{t('common:routes.home')}</MenuLink>
+      </MenuItemGroup>
+    </>
+  );
+};
+
+const AuthenticatedUserMenuContent: VFC = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  return (
+    <>
       <MenuItemGroup>
+        <MenuLink href="/account">{t('common:routes.account.index')}</MenuLink>
+      </MenuItemGroup>
+      <MenuItemGroup label={t('common:userMenu.quickLinks')}>
         <MenuLink href="/">{t('common:routes.home')}</MenuLink>
         <MenuLink href="/account/security">
           {t('common:routes.account.security')}
         </MenuLink>
         <MenuLink href="/wishlist">{t('common:routes.wishlist')}</MenuLink>
       </MenuItemGroup>
-      {isAuthenticated && (
-        <MenuItemGroup>
-          <MenuItem
-            tw="text-danger"
-            onSelect={async () => {
-              await router.push('/');
-              await AuthApi.signOut();
-            }}
-          >
-            {t('common:userMenu.logout')}
-          </MenuItem>
-        </MenuItemGroup>
-      )}
-    </Menu>
+      <MenuItemGroup>
+        <MenuItem
+          tw="text-danger"
+          onSelect={async () => {
+            await router.push('/');
+            await AuthApi.signOut();
+          }}
+        >
+          {t('common:userMenu.logout')}
+        </MenuItem>
+      </MenuItemGroup>
+    </>
   );
 };
 
@@ -69,7 +85,7 @@ const UserMenuButton = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
       >
         <Icon icon={menuFill} height={24} />
         <Icon icon={personFill} height={32} width={32} tw="p-xs ml-xs" />
-        <span tw="sr-only">Menu</span>
+        <span tw="sr-only">User menu</span>
       </ButtonGhost>
     );
   }
