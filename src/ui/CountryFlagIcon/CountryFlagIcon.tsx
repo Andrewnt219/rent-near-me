7166e969bc9a5d9d42059d1b7ed48ba6e372ca5c
Khang Nguyen <khang@khangnguyen.dev>
2021-12-13 20:31:10 -0500

    Use svg for flag icons

--- src/ui/CountryFlagIcon/CountryFlagIcon.tsx
+++ src/ui/CountryFlagIcon/CountryFlagIcon.tsx
@@ -1,6 +1,8 @@
 import { VFC } from 'react';
 import Image, { ImageProps } from 'next/image';
 import { Country } from '@utils/locale-utils';
+import UsFlag from './flags/us.svg';
+import ViFlag from './flags/vi.svg';
 
 type CountryFlagIconProps = Omit<ImageProps, 'src'> & {
   country: Country | undefined;
@@ -8,7 +10,12 @@ type CountryFlagIconProps = Omit<ImageProps, 'src'> & {
 
 const CountryFlagIcon: VFC<CountryFlagIconProps> = ({ country, ...props }) =>
   country ? (
-    <Image alt={country} {...props} src={`/images/flags/${country}.png`} />
+    <Image alt={country} {...props} src={FLAGS_BY_COUNTRY_CODE[country]} />
   ) : null;
+
+const FLAGS_BY_COUNTRY_CODE = {
+  us: UsFlag,
+  vi: ViFlag,
+};
 
 export default CountryFlagIcon;
