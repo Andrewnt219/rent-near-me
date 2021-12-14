2e0b047ffaae96dd153f216002df686d35553c83
Khang Nguyen <khang@khangnguyen.dev>
2021-12-13 20:55:59 -0500

    Add comments

--- src/ui/CountryFlagIcon/CountryFlagIcon.tsx
+++ src/ui/CountryFlagIcon/CountryFlagIcon.tsx
@@ -5,14 +5,25 @@ import UsFlag from './flags/us.svg';
 import ViFlag from './flags/vi.svg';
 
 type CountryFlagIconProps = Omit<ImageProps, 'src'> & {
+  /**
+   * The ISO 639-1 country code of the flag
+   */
   country: Country | undefined;
 };
 
+/**
+ * A component that displays the flag of the country specified by the `country` prop
+ */
 const CountryFlagIcon: VFC<CountryFlagIconProps> = ({ country, ...props }) =>
   country ? (
     <Image alt={country} {...props} src={FLAGS_BY_COUNTRY_CODE[country]} />
   ) : null;
 
+/**
+ * Key-value pairs of countries supported by this component
+ * * Key is the ISO 639-1 country code
+ * * Value can be a StaticImport or a path to the flag icon
+ */
 const FLAGS_BY_COUNTRY_CODE = {
   us: UsFlag,
   vi: ViFlag,
